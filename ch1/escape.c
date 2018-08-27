b7997b3fda9a9b3bd598a8e3651ed1d81425d8c8
Daniele Parisi <daniele@parisi.xyz>
2018-08-27 14:43:25 +0200

    escape

---
+++ ch1/escape.c
@@ -0,0 +1,17 @@
+// Exercise 1.10
+
+#include <stdio.h>
+
+void main() {
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c == '\t')
+            printf("\\t");
+        else if (c == '\b')
+            printf("\\b");
+        else if (c == '\\')
+            printf("\\\\");
+        else
+            putchar(c);
+    }
+}
