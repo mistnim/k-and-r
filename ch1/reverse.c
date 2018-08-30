bb62bca688a6f1b6b820ae27f86646380fdd491a
Daniele Parisi <daniele@parisi.xyz>
2018-08-30 11:00:14 +0200

    ex. title

--- ch1/reverse.c
+++ ch1/reverse.c
@@ -1,5 +1,5 @@
-/* Exercise 1-18 Write a program to remove trailing blanks and tabs from each line of input,
- * and to delete entirely blank lines. */
+/* Exercise 1-19. Write a function reverse(s) that reverses the character string
+ * s . Use it to write a program that reverses its input a line at a time. */
 
 
 #include <stdio.h>
